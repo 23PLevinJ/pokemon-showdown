a444c877834445dc3947735e717f6cdd02bbfe9e
23PLevinJ <23PLevinJ@qerdp.co.uk>
2026-04-03 18:06:52 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-exports.port = process.env.PORT || 10000;
+poexports.port = process.env.PORT || 10000;
 exports.bindaddress = '0.0.0.0';
 
 // Trust reverse proxies like Render
@@ -6,3 +6,4 @@ exports.proxyip = ['127.0.0.1'];
 
 // Disable built-in SSL (Render handles HTTPS)
 exports.ssl = null;
+exports.server = 'wss://ubiquitous-space-meme-v67676rgrw535p-8000.app.github.dev';
